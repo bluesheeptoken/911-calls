bcc3062ca912c95b680e41f5a4051faab96c5351
louis <louis.fruleux@etudiant.mines-nantes.fr>
2018-02-20 14:11:58 +0100

    Data correctly imported in ES

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -4,14 +4,43 @@ var fs = require('fs');
 
 var esClient = new elasticsearch.Client({
   host: 'localhost:9200',
-  log: 'error'
+  log: 'error',
+  requestTimeout: Infinity,
+  keepAlive: false
 });
+
+var emptyToNull = function(str) {
+  return (str && str.trim() !== "") ? str : null;
+}
+
+var buildLocation = function(data) {
+  var lon = emptyToNull(data["lat"]);
+  var lat = emptyToNull(data["lng"]);
+  return lon && lat ? [parseFloat(lon), parseFloat(lat)] : [0.0, 0.0];
+}
+
+let calls = [];
 
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      // TODO extract one line from CSV
+      calls.push({ "index" : { "_index" : "call", "_type" : "call" } });
+      var call = {
+          "location" : buildLocation(data),
+          "zip": parseFloat(data["zip"]),
+          "title" : (data["title"] !== undefined) ? data["title"].split(":")[0]: "",
+          "cause" : (data["title"] !== undefined) ? data["title"].split(":")[1]: "",
+          "@timestamp": new Date(data["timeStamp"]),
+          "twp": emptyToNull(data["twp"]),
+          "addr": emptyToNull(data["addr"])
+      };
+      calls.push(call);
     })
     .on('end', () => {
-      // TODO insert data to ES
+      esClient.bulk({
+        body: calls
+      }, (err, resp) => {
+        if (err) { throw err; }
+        console.log(`${resp.items.length} calls inserted`);
+      });
     });
